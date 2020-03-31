ef9a24bc25339043f63e8baf2f61c35801cde0c0
Crowdbotics <team@crowdbotics.com>
2020-03-31 03:03:57 +0000

    Update models

--- sdfsdsff_dev_2207/wsgi.py
+++ sdfsdsff_dev_2207/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sdfsdsff_dev_2207.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sdfsdsff_dev_2207.settings")
 
 application = get_wsgi_application()
